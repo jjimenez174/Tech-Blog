ab8efaa0cf63cf97d6bd1cdce43ea77ac05ce9fe
Jessica Jimenez <jjimenez174@yahoo.com>
2024-05-18 11:45:01 -0700

    fix errors

--- config/config.js
+++ config/config.js
@@ -3,10 +3,10 @@ const Sequelize = require('sequelize');
 //allow for use of .env data
 require('dotenv').config();
 
-// create connection to db, whether local on on heroku using jawsdb
+//create connection to db, whether local on on heroku using jawsdb
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PW, {
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: 'localhost',
     dialect: 'mysql',
     port: 3306
