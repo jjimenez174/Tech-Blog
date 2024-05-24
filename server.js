45285c6213636cc6e0d871c18c697fcb3543f461
Jessica Jimenez <jjimenez174@yahoo.com>
2024-05-23 18:03:47 -0700

    updated

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const sequelize = require('./config/config');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
-    secret: 'Super secret secret',
+    secret: process.env.SAM,
     cookie: {},
     resave: false,
     saveUninitialized: true,
